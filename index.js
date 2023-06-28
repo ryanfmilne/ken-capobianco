b0bf1e45371e86c6613d5fb1a8c751523c5861d6
Ryan Milne <25855081+ryanfmilne@users.noreply.github.com>
2023-06-28 15:24:52 -0700

    Lots of cool updates with sticky header

---
+++ index.js
@@ -0,0 +1,10 @@
+// Buttery Smooth Scrolling Action
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
