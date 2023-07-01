6f2113e4260d1e1a028acfb07a84ab654ea2666e
Ryan Milne <25855081+ryanfmilne@users.noreply.github.com>
2023-07-01 01:06:36 -0700

    Add GSAP mobile menu animation

--- index.js
+++ index.js
@@ -21,6 +21,111 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+// GSAP Animation Land 
+// var tlPage = gsap.timeline();
+// tlPage.from("body", {opacity: 0 , duration: 3});
 
-var tl = gsap.timeline();
-tl.from("body", {opacity: 0 , duration: 3});
+
+// GSAP mobile nav
+
+// set initial menu state
+let menuState = "closed";
+
+// When the burger menu is clicked
+document.querySelector(".burg").addEventListener("click", function(){
+    if(menuState == "closed") {
+        openMenu();
+        console.log(menuState);
+    } else if (menuState == "open") {
+        closeMenu();
+        console.log(menuState);
+    }
+});
+
+// When the overlay is clicked
+document.querySelector(".mobile-overlay").addEventListener("click", function(){
+    if (menuState == "open") {
+        closeMenu();
+        console.log(menuState);
+    }
+});
+
+// When the links are clicked
+
+
+// const links = Array.from(document.querySelectorAll('.nav-links a'));
+// for (let  i = 0; i < links.length; i++) {
+//     links[i].addEventListener("click", function() {
+//         closeMenu();
+//     });
+// }
+
+
+
+
+
+// Open menu animation
+function openMenu() {
+    let tl = gsap.timeline();
+    tl.to(".mobile-overlay", {display: "block", duration: 0});
+    tl.to("#nav-menu", {maxHeight: 400, duration: .5});
+    
+    tl.to(".mobile-overlay", {opacity: 1, duration: .5}, "-=.5");
+    menuState = "open";
+}
+
+// Close menu animation
+function closeMenu() {
+    let tl = gsap.timeline();
+    tl.to(".mobile-overlay", {opacity: 0, duration: .5});
+    tl.to(".mobile-overlay", {display: "none", duration: 0});
+    tl.to("#nav-menu", {maxHeight: 0}, "-=.5");
+    
+    
+    menuState = "closed";
+}
+
+
+
+// ///////////////////////////////////////////////////// Vanilla solution
+// /////////////////////////////////////////////////////
+// Mobile Menu - someone working vanilla solution 
+
+// let burger = document.querySelector(".burg");
+// burger.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//     var x = document.getElementById("nav-menu");
+//     // if menu is open
+//     if (x.style.maxHeight === "400px") { 
+//       // close it
+//       x.style.maxHeight = "0";
+//       document.querySelector(".mobile-overlay").style.opacity = "0"
+//       document.querySelector(".mobile-overlay").style.display = "none"
+//       menuStatus = "closed"
+//       console.log(menuStatus);
+    
+//     } else {
+//       // if it's not open
+//       x.style.maxHeight = "400px";
+//       document.querySelector(".mobile-overlay").style.display = "block"
+//       document.querySelector(".mobile-overlay").style.opacity = "1"
+//       menuStatus = "open"
+//       console.log(menuStatus);
+//         if(menuStatus == "open") {
+//             let overlay = document.querySelector(".mobile-overlay");
+//             overlay.addEventListener("click", toggleMenu);
+
+//             let links = document.querySelectorAll("nav a");
+            
+//             for (i = 0; i < links.length; i ++) {
+//                 console.log(links[i]);
+//                 links[i].addEventListener("click", toggleMenu);
+//             }
+//         }
+//       }
+//     }
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
